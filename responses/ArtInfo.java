fd5f6fa4d30ae184907723a0134f2b59520f0e85
Doreenyang <98991516+Doreenyang@users.noreply.github.com>
2025-09-29 21:29:17 -0500

    update file structure

---
+++ responses/ArtInfo.java
@@ -0,0 +1,19 @@
+
+
+public class ArtInfo {
+    private String id;
+    private float weight;
+
+    public ArtInfo(String id, float weight) {
+        this.id = id;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
