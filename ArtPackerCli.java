d0a892365e34db75c5adc9517f21987e67e3aae6
Doreenyang <98991516+Doreenyang@users.noreply.github.com>
2025-10-09 21:09:17 -0500

    update response

--- ArtPackerCli.java
+++ ArtPackerCli.java
@@ -1,17 +1,10 @@
-// No package declaration since it's in the root source folder
-
 import entities.Art;
 import interactor.Packer;
-import java.util.ArrayList;
 import java.util.List;
 import parser.CSVParser;
-import requests.Request;
-import responses.ArtInfo;
-import responses.BoxInfo;
-import responses.ContainerInfo;
 import responses.Response;
 
-public class ArtPackerCli { // <-- Changed from HelloWorld to ArtPackerCli
+public class ArtPackerCli {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please provide the CSV file name as a command line argument.");
@@ -19,58 +12,16 @@ public class ArtPackerCli { // <-- Changed from HelloWorld to ArtPackerCli
         }
         String fileName = args[0];
 
+        // Parse CSV and create art objects
         CSVParser parser = new CSVParser();
         List<Art> arts = parser.parse(fileName);
 
-        Request packRequest = new Request(arts, true);
-
-        Packer packer = new Packer(packRequest.getArtObjects());
+        // Pack the items
+        Packer packer = new Packer(arts);
         packer.pack();
 
-        // Create the response object
-        List<ArtInfo> itemInfos = new ArrayList<>();
-        for (Art art : packer.getArtsToPack()) {
-            itemInfos.add(new ArtInfo(art.getId(), art.getWeight()));
-        }
-
-        List<BoxInfo> boxInfos = new ArrayList<>();
-        packer.getBoxesUsed().forEach(box -> {
-            List<String> artIds = new ArrayList<>();
-            box.getArtsInBox().forEach(art -> artIds.add(art.getId()));
-            boxInfos.add(new BoxInfo(box.getId(), artIds, box.getWeight()));
-        });
-
-        List<ContainerInfo> containerInfos = new ArrayList<>();
-        packer.getContainersUsed().forEach(container -> {
-            List<String> boxIds = new ArrayList<>();
-            container.getBoxInContainer().forEach(box -> boxIds.add(box.getId()));
-            String type = container.isCrate() ? "Crate" : "Pallet";
-            containerInfos.add(new ContainerInfo(container.getId(), boxIds, container.getTotalWeight(), 0, type));
-        });
-
-        float totalWeight = 0;
-        for(ContainerInfo container : containerInfos) {
-            totalWeight += container.getWeight();
-        }
-
-        Response response = new Response(itemInfos, boxInfos, containerInfos, totalWeight);
-
-        // Dump the response to the terminal
-        System.out.println("--- Shipment Details ---");
-        System.out.println("Total Weight: " + response.getTotalWeight());
-        System.out.println("\n--- Items ---");
-        response.getArts().forEach(item -> {
-            System.out.println("ID: " + item.getId() + ", Weight: " + item.getWeight());
-        });
-        System.out.println("\n--- Boxes ---");
-        response.getBoxes().forEach(box -> {
-            System.out.println("ID: " + box.getId() + ", Weight: " + box.getWeight());
-            System.out.println("  Items: " + String.join(", ", box.getItemIds()));
-        });
-        System.out.println("\n--- Containers ---");
-        response.getContainers().forEach(container -> {
-            System.out.println("ID: " + container.getId() + ", Type: " + container.getType() + ", Weight: " + container.getWeight() + ", Height: " + container.getHeight());
-            System.out.println("  Boxes: " + String.join(", ", container.getBoxIds()));
-        });
+        // Create response and display results
+        Response response = new Response(packer);
+        response.displayShipmentDetails();
     }
 }
--- responses/Response.java
+++ responses/Response.java
@@ -1,51 +1,82 @@
 package responses;
 
+import entities.Art;
+import entities.Box;
+import entities.Container;
+import interactor.Packer;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Response
-{
-    private List<ArtInfo> arts;
-    private List<BoxInfo> boxes;
-    private List<ContainerInfo> containers;
+public class Response {
+    private List<Art> arts;
+    private List<Box> boxes;
+    private List<Container> containers;
     private float totalWeight;
 
-    public Response(List<ArtInfo> arts, List<BoxInfo> boxes, List<ContainerInfo> containers, float totalWeight) {
-        this.arts = arts;
-        this.boxes = boxes;
-        this.containers = containers;
-        this.totalWeight = totalWeight;
+    public Response(Packer packer) {
+        this.arts = packer.getArtsToPack();
+        this.boxes = packer.getBoxesUsed();
+        this.containers = packer.getContainersUsed();
+        this.totalWeight = calculateTotalWeight();
     }
 
-    public List<ArtInfo> getArts() {
+    private float calculateTotalWeight() {
+        float weight = 0;
+        for (Container container : containers) {
+            weight += container.getTotalWeight();
+        }
+        return weight;
+    }
+
+    public void displayShipmentDetails() {
+        System.out.println("--- Shipment Details ---");
+        System.out.println("Total Weight: " + totalWeight);
+        
+        System.out.println("\n--- Items ---");
+        for (Art art : arts) {
+            System.out.println("ID: " + art.getId() + ", Weight: " + art.getWeight());
+        }
+        
+        System.out.println("\n--- Boxes ---");
+        for (Box box : boxes) {
+            System.out.println("ID: " + box.getId() + ", Weight: " + box.getWeight());
+            System.out.print("  Items: ");
+            List<String> itemIds = new ArrayList<>();
+            for (Art art : box.getArtsInBox()) {
+                itemIds.add(art.getId());
+            }
+            System.out.println(String.join(", ", itemIds));
+        }
+        
+        System.out.println("\n--- Containers ---");
+        for (Container container : containers) {
+            String type = container.isCrate() ? "Crate" : "Pallet";
+            System.out.println("ID: " + container.getId() + ", Type: " + type + 
+                             ", Weight: " + container.getTotalWeight() + 
+                             ", Height: " + container.getHeight());
+            System.out.print("  Boxes: ");
+            List<String> boxIds = new ArrayList<>();
+            for (Box box : container.getBoxInContainer()) {
+                boxIds.add(box.getId());
+            }
+            System.out.println(String.join(", ", boxIds));
+        }
+    }
+
+    // Getters for the response data
+    public List<Art> getArts() {
         return arts;
     }
 
-    public List<BoxInfo> getBoxes() {
+    public List<Box> getBoxes() {
         return boxes;
     }
 
-    public List<ContainerInfo> getContainers() {
+    public List<Container> getContainers() {
         return containers;
     }
 
     public float getTotalWeight() {
         return totalWeight;
-    }
-
-
-    public void setArts(List<ArtInfo> arts) {
-        this.arts = arts;
-    }
-
-    public void setBoxes(List<BoxInfo> boxes) {
-        this.boxes = boxes;
-    }
-
-    public void setContainers(List<ContainerInfo> containers) {
-        this.containers = containers;
-    }
-
-    public void setTotalWeight(float totalWeight) {
-        this.totalWeight = totalWeight;
     }
 }
