5e55366b708c26458f85319603319e142e8341eb
Doreenyang <98991516+Doreenyang@users.noreply.github.com>
2025-10-29 20:20:37 -0500

    update test pallet mixed_box

---
+++ app/src/test/java/archdesign/integration/PalletPackingLargeBox4Test.java
@@ -0,0 +1,67 @@
+package archdesign.integration;
+
+import archdesign.Main;
+import archdesign.response.ArtViewModel;
+import archdesign.response.BoxViewModel;
+import archdesign.response.ContainerViewModel;
+import archdesign.response.ShipmentViewModel;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PalletPackingLargeBox4Test {
+
+    @Test
+    void testLargeBox4_matchesExpectedJson() throws Exception {
+        URL inUrl = getClass().getResource("/pallet_packing/standard_pallet/large_box/large_box_4/input.csv");
+        assertNotNull(inUrl, "Input resource missing");
+        Path inPath = Paths.get(inUrl.toURI());
+
+        ShipmentViewModel vm = Main.processFile(inPath.toString());
+        assertNotNull(vm, "ShipmentViewModel should not be null");
+
+        int totalPieces = 0;
+        int standardPalletCount = 0;
+        int oversizedPalletCount = 0;
+        int crateCount = 0;
+
+        for (ContainerViewModel container : vm.containers()) {
+            String t = container.type();
+            if ("STANDARD_PALLET".equals(t)) standardPalletCount++;
+            else if ("OVERSIZE_PALLET".equals(t)) oversizedPalletCount++;
+            else if ("STANDARD_CRATE".equals(t)) crateCount++;
+
+            for (BoxViewModel box : container.boxes()) {
+                for (ArtViewModel art : box.arts()) {
+                    totalPieces++;
+                }
+            }
+        }
+
+        URL expectedUrl = getClass().getResource("/pallet_packing/standard_pallet/large_box/large_box_4/expected_output.json");
+        assertNotNull(expectedUrl, "expected_output.json missing");
+        Path expectedPath = Paths.get(expectedUrl.toURI());
+        String json = Files.readString(expectedPath);
+        JsonObject obj = new Gson().fromJson(json, JsonObject.class);
+
+        if (obj.has("total_pieces")) {
+            assertEquals(obj.get("total_pieces").getAsInt(), totalPieces, "total_pieces mismatch");
+        }
+        if (obj.has("standard_pallet_count")) {
+            assertEquals(obj.get("standard_pallet_count").getAsInt(), standardPalletCount, "standard_pallet_count mismatch");
+        }
+        if (obj.has("oversized_pallet_count")) {
+            assertEquals(obj.get("oversized_pallet_count").getAsInt(), oversizedPalletCount, "oversized_pallet_count mismatch");
+        }
+        if (obj.has("crate_count")) {
+            assertEquals(obj.get("crate_count").getAsInt(), crateCount, "crate_count mismatch");
+        }
+    }
+}
